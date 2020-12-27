5286f0d09b54281e47668f40c75ee97aed275d50
jwonnyleaf <jwonnyleaf@gmail.com>
2020-12-26 20:39:14 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CloudBot
 This is a development bot/project for the discord server, Faded.
+
+Faded Staff Team
--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('faded'):
+    if message.content.startswith('Faded'):
         await message.channel.send('is cool')
 
 client.run('NzA0ODU4Mjg3NDE3MzI3NzU4.XqjQbw.LC3yJMD1cz8iosQRhEt0iwtU60c')
